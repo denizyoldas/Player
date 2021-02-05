7d64ae279f057e26be49a58b5b2446af0e07e40f
denizyoldas <36186348+denizyoldas@users.noreply.github.com>
2021-02-06 00:51:34 +0300

    Add project files.

---
+++ Player/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Player
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            customizeDesign();
+        }
+
+        private void customizeDesign()
+        {
+            panelMediaSubmenu.Visible = false;
+            panelPlaylistSubMenu.Visible = false;
+            panelToolsSubMenu.Visible = false;
+            //..
+        }
+
+        private void hideSubMenu()
+        {
+            if (panelMediaSubmenu.Visible)
+                panelMediaSubmenu.Visible = false;
+            if (panelPlaylistSubMenu.Visible)
+                panelPlaylistSubMenu.Visible = false;
+            if (panelToolsSubMenu.Visible)
+                panelToolsSubMenu.Visible = false;
+        }
+
+        private void showSubMenu(Panel subMenu)
+        {
+            if (subMenu.Visible == false)
+            {
+                hideSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+            {
+                subMenu.Visible = false;
+            }
+        }
+
+        private void btnMedia_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelMediaSubmenu);
+        }
+        #region MediaSubMenu
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openChildForm(new Form2());
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+#endregion
+        private void btnPlaylist_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelPlaylistSubMenu);
+        }
+        #region PlaylistSubMenu
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void btnEqualizer_Click(object sender, EventArgs e)
+        {
+            openChildForm(new Form3());
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+#endregion
+        private void btnTools_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelToolsSubMenu);
+        }
+        #region ToolsSubMenu
+        private void button15_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+#endregion
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            //..
+            //your codes
+            //..
+            hideSubMenu();
+        }
+
+        private Form activeForm = null;
+        private void openChildForm(Form childForm)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelChildForm.Controls.Add(childForm);
+            panelChildForm.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+    }
+}
